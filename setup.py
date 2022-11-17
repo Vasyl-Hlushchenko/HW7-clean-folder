969a357e365d658662fc0106aba15b80d41a5948
Vasyl Hlushchenko <Gluschenkov88@gmail.com>
2022-11-17 21:06:21 +0200

    upp

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='clean_folder',
     version='1.0',
-    entry_points={'console_scripts': ['clean-folder = clean_folder.clean:get_main_path']},
+    entry_points={'console_scripts': ['clean = clean_folder.clean:get_main_path']},
     description='Clean folder script',
     author='Hlushchenko Vasiliy',
     author_email='Gluschenkov88@gmail.com',
